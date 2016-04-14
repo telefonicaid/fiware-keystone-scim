6757e2f3d9c4d3d74ad244b455712fd83fb10c07
magarciasopo <magarciasopo@gmail.com>
2016-04-14 12:19:27 +0200

    Customize for Keystone-scim component

--- get_version_string.sh
+++ get_version_string.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 # Copyright 2013 Telefonica Investigacion y Desarrollo, S.A.U
 #
-# This file is part of fiware-cygnus (FI-WARE project).
+# This file is part of fiware-keystone-scim (FI-WARE project).
 #
-# fiware-cygnus is free software: you can redistribute it and/or
+# fiware-keystone-scim is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# fiware-cygnus is distributed in the hope that it will be useful,
+# fiware-keystone-scim is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 # General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with fiware-cygnus. If not, see http://www.gnu.org/licenses/.
+# along with fiware-keystone-scim. If not, see http://www.gnu.org/licenses/.
 #
 # For those usages not covered by this license please contact with
 # iot_support at tid dot es
