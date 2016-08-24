17892d3042ed36c2e9820475bf6d0ed06d6b38d7
Alvaro Vega <avega@tid.es>
2016-08-24 07:55:48 +0200

    add with_python27 option

--- package-keystone-scim.sh
+++ package-keystone-scim.sh
@@ -11,11 +11,19 @@ string=$(get_rpm_version_string)
 ver=${string% *}
 rel=${string#* }
 
+PYTHON27_VALUE=0
+PYTHON27_ARG=${1}
+
+if [ "$PYTHON27_ARG" == "with_python27" ]; then
+    PYTHON27_VALUE=1
+fi
+
 RPM_DIR=$BASE/build/rpm
 mkdir -p $RPM_DIR/BUILD
 
 rpmbuild -bb keystone-scim.spec \
-  --define "_topdir $RPM_DIR" \
+  --define "_topdir  $RPM_DIR" \
   --define "_root $BASE"\
   --define "_version $ver"\
-  --define "_release $rel"  
+  --define "_release $rel"\
+  --define "with_python27 $PYTHON27_VALUE"
