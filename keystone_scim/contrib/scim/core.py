a448bf79064f9c0ad797723f67619613c08237d7
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2020-04-07 10:46:48 +0200

    disable extensionn stuff

--- keystone_scim/contrib/scim/core.py
+++ keystone_scim/contrib/scim/core.py
@@ -18,7 +18,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from keystone.common import extension
+#from keystone.common import extension
 
 
 EXTENSION_DATA = {
@@ -36,8 +36,8 @@ EXTENSION_DATA = {
     ]
 }
 
-extension.register_admin_extension(EXTENSION_DATA['alias'], EXTENSION_DATA)
-extension.register_public_extension(EXTENSION_DATA['alias'], EXTENSION_DATA)
+#extension.register_admin_extension(EXTENSION_DATA['alias'], EXTENSION_DATA)
+#extension.register_public_extension(EXTENSION_DATA['alias'], EXTENSION_DATA)
 
 # Monkey patch SQL pagination
 
